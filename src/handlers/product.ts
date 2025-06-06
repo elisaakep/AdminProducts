e3f6544ca78a4cb0a2b7e25bb0464e14b5b79709
Elisaak <escobar.abraham.p1m@gmail.com>
2025-06-06 03:14:22 -0600

    toVid384

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -6,11 +6,25 @@ export const getProducts = async  (req: Request, res: Response) => {
         const products = await Product.findAll({
             order: [
                 ['id', 'DESC']
-            ]
-            // ,attributes: {exclude: ['createdAt', 'updatedAt', 'availability']}
-            //, limit: 2
+            ]  
         })
         res.json({data: products})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getProductById = async  (req: Request, res: Response) => {
+    try {
+        const { id } = req.params
+        const product = await Product.findByPk(id)
+
+        if(!product){
+            // return res.status(404).json({
+            //     error: 'Producto no encontrado'
+            // })
+        }
+        res.json({data: product})
     } catch (error) {
         console.log(error)
     }
@@ -24,3 +38,54 @@ export const createProduct = async (req: Request, res: Response) =>{
         console.log(error)
     }
 }
+
+export const updateProduct = async (req: Request, res: Response) => {
+    const { id } = req.params
+        const product = await Product.findByPk(id)
+
+        if(!product){
+            // return res.status(404).json({
+            //     error: 'Producto no encontrado'
+            // })
+        }
+
+        //Actualizar
+        await product.update(req.body)
+        await product.save()
+        
+        res.json({data: product})
+}
+
+
+export const updateAvailability = async (req: Request, res: Response) => {
+    const { id } = req.params
+        const product = await Product.findByPk(id)
+
+        if(!product){
+            // return res.status(404).json({
+            //     error: 'Producto no encontrado'
+            // })
+        }
+        //Actualizar
+        product.availability = !product.dataValues.availability
+        await product.save()
+        res.json({data: product})
+}
+
+export const deleteProduct = async (req: Request, res: Response) => {
+    const { id } = req.params
+        const product = await Product.findByPk(id)
+
+        if(!product){
+            // return res.status(404).json({
+            //     error: 'Producto no encontrado'
+            // })
+        }
+
+        await product.destroy()
+        res.json({data: 'Producto eliminado'})
+
+}
+
+// ,attributes: {exclude: ['createdAt', 'updatedAt', 'availability']}
+            //, limit: 2
--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express'
-import { body } from 'express-validator'
-import { createProduct, getProducts } from './handlers/product'
+import { body, param } from 'express-validator'
+import { createProduct, deleteProduct, getProductById, getProducts, updateAvailability, updateProduct } from './handlers/product'
 import { handleInputErrors } from './middleware'
-//import { ExpressValidator } from 'express-validator'
+
 const router = Router()
 
 //Routing
 router.get('/', getProducts)
+router.get('/:id', 
+    param('id').isInt().withMessage('ID no valido'),
+    handleInputErrors,
+    getProductById)
 
 router.post('/',
     //validacion
@@ -20,16 +24,28 @@ router.post('/',
     createProduct
 )
 
-router.put('/', (req, res) => {
-    res.json('Desde PUT')
-})
+router.put('/:id', 
+    param('id').isInt().withMessage('ID no valido'),
+    body('name')
+        .notEmpty().withMessage('El nombre del producto esta vacio'),
+    body('price')
+        .isNumeric().withMessage('Valor no valido')
+        .notEmpty().withMessage('No Valido')
+        .custom(value => value > 0).withMessage('Precio no valido'),
+        body('availability')
+        .isBoolean().withMessage('Valor para disponibilidad no valido'),
+        handleInputErrors,
+    updateProduct)
 
-router.patch('/', (req, res) => {
-    res.json('Desde PATCH')
-})
+router.patch('/:id', 
+    param('id').isInt().withMessage('ID no valido'),
+    handleInputErrors,
+    updateAvailability)
 
-router.delete('/', (req, res) => {
-    res.json('Desde DELETE')
-})
+router.delete('/:id', 
+    param('id').isInt().withMessage('ID no valido'),
+    handleInputErrors,
+    deleteProduct
+)
 
 export default router
